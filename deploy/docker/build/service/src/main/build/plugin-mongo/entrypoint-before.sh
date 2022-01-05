4d7dd3b19c527794308bb589617a99794137f644
Frank Thomschke <thomschke@edu-sharing.net>
2022-01-05 09:32:46 +0100

    bugfix compose

--- deploy/docker/build/service/src/main/build/plugin-mongo/entrypoint-before.sh
+++ deploy/docker/build/service/src/main/build/plugin-mongo/entrypoint-before.sh
@@ -5,7 +5,5 @@ echo "plugin-mongo: entrypoint-before"
 
 repository_mongo_host="${REPOSITORY_MONGO_HOST:-repository-mongo}"
 repository_mongo_port="${REPOSITORY_MONGO_PORT:-27017}"
-repository_mongo_user="${REPOSITORY_MONGO_USER:-repository}"
-repository_mongo_pass="${REPOSITORY_MONGO_PASS:-repository}"
 
 until wait-for-it "${repository_mongo_host}:${repository_mongo_port}" -t 3; do sleep 1; done
