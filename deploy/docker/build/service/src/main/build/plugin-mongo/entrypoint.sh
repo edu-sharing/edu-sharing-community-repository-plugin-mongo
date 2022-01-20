8e9b8e4d8f3fb87434c3121620645934072f4b48
wollner <wollner@edu-sharing.net>
2022-01-20 11:12:21 +0100

    cleanup deploy

--- deploy/docker/build/service/src/main/build/plugin-mongo/entrypoint.sh
+++ deploy/docker/build/service/src/main/build/plugin-mongo/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eux
 
+repository_mongo_protocol="${REPOSITORY_MONGO_PROTOCOL:-mongodb}"
 repository_mongo_host="${REPOSITORY_MONGO_HOST:-repository-mongo}"
 repository_mongo_port="${REPOSITORY_MONGO_PORT:-27017}"
 repository_mongo_user="${REPOSITORY_MONGO_USER:-repository}"
 repository_mongo_pass="${REPOSITORY_MONGO_PASS:-repository}"
+repository_mongo_database="${REPOSITORY_MONGO_DATABASE:-edu-sharing}"
 
 eduSConf="tomcat/shared/classes/config/cluster/edu-sharing.deployment.conf"
 
@@ -17,10 +19,7 @@ until wait-for-it "${repository_mongo_host}:${repository_mongo_port}" -t 3; do s
 ### edu-sharing platform ###############################################################################################
 
 hocon -f "${eduSConf}" \
-	set "mongo.servers" '["'"${repository_mongo_host}:${repository_mongo_port}"'"]'
+	set "mongo.connectionString" '["'"${repository_mongo_protocol}://${repository_mongo_user}:${repository_mongo_pass}@${repository_mongo_host}:${repository_mongo_port}"'"]'
 
 hocon -f "${eduSConf}" \
-	set "mongo.username" "${repository_mongo_user}"
-
-hocon -f "${eduSConf}" \
-	set "mongo.password" "${repository_mongo_pass}"
+	set "mongo.database" "${repository_mongo_database}"
