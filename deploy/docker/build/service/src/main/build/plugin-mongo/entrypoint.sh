88b76cc710b29be8fa85a827814f81ef3f592b5a
wollner <wollner@edu-sharing.net>
2022-02-04 16:32:04 +0100

    feat: docker: added log level for plugin-mongo on debugging mode

--- deploy/docker/build/service/src/main/build/plugin-mongo/entrypoint.sh
+++ deploy/docker/build/service/src/main/build/plugin-mongo/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux
 
+
+debug_repository_plugin_mongo="${DEBUG_REPOSITORY_PLUGIN_MONGO:-}"
 repository_mongo_protocol="${REPOSITORY_MONGO_PROTOCOL:-mongodb}"
 repository_mongo_host="${REPOSITORY_MONGO_HOST:-repository-mongo}"
 repository_mongo_port="${REPOSITORY_MONGO_PORT:-27017}"
@@ -9,6 +11,19 @@ repository_mongo_pass="${REPOSITORY_MONGO_PASS:-repository}"
 repository_mongo_database="${REPOSITORY_MONGO_DATABASE:-edu-sharing}"
 
 eduSConf="tomcat/shared/classes/config/cluster/edu-sharing.deployment.conf"
+
+
+### Enable Debug Logs ##################################################################################################
+if [[ "${debug_repository_plugin_mongo}" = 'true' ]] ; then
+  echo "Enable log level debug for repository-plugin-mongo"
+
+  xmlstarlet ed -L \
+  --subnode '/Configuration/Loggers' --type elem -n Logger -v "" \
+  --var node '$prev' \
+  -i '$node' -t attr -n 'name' -v 'org.edu-sharing.plugin_mongo' \
+  -i '$node' -t attr -n 'level' -v 'debug' \
+  'tomcat/webapps/edu-sharing/WEB-INF/log4j2.xml'
+fi
 
 ### Wait ###############################################################################################################
 
@@ -19,7 +34,7 @@ until wait-for-it "${repository_mongo_host}:${repository_mongo_port}" -t 3; do s
 ### edu-sharing platform ###############################################################################################
 
 hocon -f "${eduSConf}" \
-	set "mongo.connectionString" '["'"${repository_mongo_protocol}://${repository_mongo_user}:${repository_mongo_pass}@${repository_mongo_host}:${repository_mongo_port}"'"]'
+	set "mongo.connectionString" '"'"${repository_mongo_protocol}://${repository_mongo_user}:${repository_mongo_pass}@${repository_mongo_host}:${repository_mongo_port}"'"'
 
 hocon -f "${eduSConf}" \
 	set "mongo.database" "${repository_mongo_database}"
