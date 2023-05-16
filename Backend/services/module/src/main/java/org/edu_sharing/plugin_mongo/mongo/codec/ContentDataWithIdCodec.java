a8ed41e156469863fd63f0165ac1247f6676e68b
Torsten Simon <simon@edu-sharing.net>
2023-05-16 11:47:55 +0200

    fix:content data codec return null value

--- Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/mongo/codec/ContentDataWithIdCodec.java
+++ Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/mongo/codec/ContentDataWithIdCodec.java
@@ -1,21 +1,29 @@
 package org.edu_sharing.plugin_mongo.mongo.codec;
 
 import org.alfresco.repo.domain.node.ContentDataWithId;
-import org.apache.commons.lang3.NotImplementedException;
 import org.bson.BsonReader;
 import org.bson.BsonWriter;
+import org.bson.BsonType;
 import org.bson.codecs.Codec;
 import org.bson.codecs.DecoderContext;
 import org.bson.codecs.EncoderContext;
+import org.edu_sharing.plugin_mongo.mongo.MongoInvalidTypeException;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class ContentDataWithIdCodec implements Codec<ContentDataWithId> {
     @Override
-    public ContentDataWithId decode(BsonReader bsonReader, DecoderContext decoderContext) {
-        throw new NotImplementedException("ContentDataWithId can not be decoded");
-    }
+    public ContentDataWithId decode(BsonReader reader, DecoderContext decoderContext) {
+        switch (reader.getCurrentBsonType()) {
+            case STRING:
+                return null;
+            default:
+                throw new MongoInvalidTypeException(String.format("%s can not create ContentDataWithId from type %s", reader.getCurrentName(), reader.getCurrentBsonType()));
 
+        }
+    }
     @Override
     public void encode(BsonWriter bsonWriter, ContentDataWithId contentDataWithId, EncoderContext encoderContext) {
         bsonWriter.writeString(contentDataWithId.toString());
