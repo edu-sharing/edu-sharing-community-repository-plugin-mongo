0e01871f97d1d16d9a61d3d102f1f7f9e898a432
Torsten Simon <simon@edu-sharing.net>
2023-05-16 17:44:13 +0200

    fix:content data codec basic decoding

--- Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/mongo/codec/LocaleCodec.java
+++ Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/mongo/codec/LocaleCodec.java
@@ -3,23 +3,25 @@ package org.edu_sharing.plugin_mongo.mongo.codec;
 import org.alfresco.repo.domain.node.ContentDataWithId;
 import org.alfresco.service.cmr.repository.ContentData;
 import org.bson.BsonReader;
-import org.bson.BsonType;
 import org.bson.BsonWriter;
+import org.bson.BsonType;
 import org.bson.codecs.Codec;
 import org.bson.codecs.DecoderContext;
 import org.bson.codecs.EncoderContext;
 import org.edu_sharing.plugin_mongo.mongo.MongoInvalidTypeException;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 
 @Component
-public class ContentDataWithIdCodec implements Codec<ContentDataWithId> {
+public class LocaleCodec implements Codec<Locale> {
     @Override
-    public ContentDataWithId decode(BsonReader reader, DecoderContext decoderContext) {
+    public Locale decode(BsonReader reader, DecoderContext decoderContext) {
         if (Objects.requireNonNull(reader.getCurrentBsonType()) == BsonType.DOCUMENT) {
             reader.readStartDocument();
-            String contentUrl = "", mimetype = "", encoding = "";
+            String language = "", country = "", variant = "";
             long size = 0;
             while (true) {
                 BsonType bsonType = reader.readBsonType();
@@ -30,35 +32,30 @@ public class ContentDataWithIdCodec implements Codec<ContentDataWithId> {
                 String name = reader.readName();
                 if (bsonType == BsonType.STRING) {
                     switch (name) {
-                        case "contentUrl":
-                            contentUrl = reader.readString();
+                        case "language":
+                            language = reader.readString();
                             break;
-                        case "mimetype":
-                            mimetype = reader.readString();
+                        case "country":
+                            country = reader.readString();
                             break;
-                        case "encoding":
-                            encoding = reader.readString();
+                        case "variant":
+                            variant = reader.readString();
                             break;
-                    }
-                } else if(bsonType == BsonType.INT64) {
-                     if (name.equals("size")) {
-                        size = reader.readInt64();
                     }
                 }
             }
-            return new ContentDataWithId(new ContentData(contentUrl, mimetype, size, encoding), null);
+            return new Locale(language, country, variant);
         }
         throw new MongoInvalidTypeException(String.format("%s can not create ContentDataWithId from type %s", reader.getCurrentName(), reader.getCurrentBsonType()));
     }
     @Override
-    public void encode(BsonWriter bsonWriter, ContentDataWithId contentDataWithId, EncoderContext encoderContext) {
+    public void encode(BsonWriter bsonWriter, Locale locale, EncoderContext encoderContext) {
         // @TODO
-        // bsonWriter.writeString(contentDataWithId.toString());
     }
 
     @Override
-    public Class<ContentDataWithId> getEncoderClass() {
-        return ContentDataWithId.class;
+    public Class<Locale> getEncoderClass() {
+        return Locale.class;
     }
 }
 
