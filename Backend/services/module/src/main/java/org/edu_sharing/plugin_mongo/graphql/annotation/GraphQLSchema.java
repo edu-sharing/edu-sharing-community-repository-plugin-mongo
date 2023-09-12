7508b18d598e51684eebab200e3d4ea9fbce14e9
Sebastian Wollner <wollner@edu-sharing.net>
2023-09-12 14:33:27 +0200

    fix: missing runtime type annotation for GraphQLSchema annotation

--- Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/graphql/annotation/GraphQLSchema.java
+++ Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/graphql/annotation/GraphQLSchema.java
@@ -1,11 +1,16 @@
 package org.edu_sharing.plugin_mongo.graphql.annotation;
 
 
+import java.lang.annotation.*;
+
 /**
  * Registers a type for GraphQL schema definition.
  * Mainly used for polymorphic types like a GraphQL type that implements an interface
  * Or if classname and model name diverges
  */
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface GraphQLSchema {
 
     /**
