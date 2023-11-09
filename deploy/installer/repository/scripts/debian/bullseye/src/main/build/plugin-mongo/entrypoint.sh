f58f53f65fc8c7113e67e1ddb6c427ee94252a49
Sebastian Wollner <wollner@edu-sharing.net>
2023-11-09 09:37:06 +0100

    cleanup: removed legacy installer

--- deploy/installer/repository/scripts/debian/bullseye/src/main/build/plugin-mongo/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-repository_mongo_db_connection_string="${REPOSITORY_MONGO_CONNECTION_STRING}"
-
-if [[ -z $repository_mongo_db_connection_string ]] ; then
-  echo "connectionString not setup!"
-  exit 1;
-fi
