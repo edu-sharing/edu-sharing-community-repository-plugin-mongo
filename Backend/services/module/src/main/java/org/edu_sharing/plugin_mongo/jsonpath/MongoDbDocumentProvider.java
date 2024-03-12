69109d441556a91a9e560dbd285fd05cabf4814b
Daniel Rudolph <rudolph@edu-sharing.net>
2024-03-12 14:04:29 +0100

    Merge remote-tracking branch 'origin/maven/fixes/7.0_merge_bp_security' into maven/fixes/8.3

--- Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/jsonpath/MongoDbDocumentProvider.java
+++ Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/jsonpath/MongoDbDocumentProvider.java
@@ -3,17 +3,12 @@ package org.edu_sharing.plugin_mongo.jsonpath;
 import com.jayway.jsonpath.InvalidJsonException;
 import com.jayway.jsonpath.JsonPathException;
 import com.jayway.jsonpath.spi.json.AbstractJsonProvider;
-import com.jayway.jsonpath.spi.json.JsonProvider;
 import org.apache.commons.io.IOUtils;
 import org.bson.Document;
-import org.yaml.snakeyaml.reader.StreamReader;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Collection;
 
 public class MongoDbDocumentProvider extends AbstractJsonProvider {
 
