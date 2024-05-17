941d8435b7dfe79908e4ff46d1279925194af681
Sebastian Wollner <wollner@edu-sharing.net>
2024-05-17 13:46:46 +0200

    update dependencies

--- Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/domain/metadata/Repository.java
+++ Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/domain/metadata/Repository.java
@@ -3,7 +3,6 @@ package org.edu_sharing.plugin_mongo.domain.metadata;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import javax.validation.constraints.NotBlank;
 
 @Data
 @SuperBuilder
