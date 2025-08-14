10bd2cd1e44f51ba02a795520f9a475cc3eaec56
Sebastian Wollner <wollner@edu-sharing.net>
2025-08-14 13:52:10 +0200

    feat: add index to userId and update compound index in UserNodeActivityData
    Enhanced indexing in `UserNodeActivityData` by adding an individual index for `userId` and updating the compound index to use `username`. This improves query optimization and retrieval performance.

--- Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/user_activity/UserNodeActivityData.java
+++ Backend/services/module/src/main/java/org/edu_sharing/plugin_mongo/user_activity/UserNodeActivityData.java
@@ -7,6 +7,7 @@ import nonapi.io.github.classgraph.json.Id;
 import org.edu_sharing.plugin_mongo.oplog.MongoAlfOpLogData;
 import org.edu_sharing.service.tracking.user_tracking.UserNodeActivity;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -15,11 +16,12 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document("user_node_events")
-@CompoundIndex(name = "timestamp_userId_idx", def = "{'timestamp': -1, 'userId': 1}")
+@CompoundIndex(name = "timestamp_username_idx", def = "{'timestamp': -1, 'username': 1}")
 public class UserNodeActivityData implements MongoAlfOpLogData, UserNodeActivity {
     @Id
     String id;
     String nodeId;
+    @Indexed
     String userId;
     String username;
     String type;
